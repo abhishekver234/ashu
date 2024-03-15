fd294f79012db13b76b5b6d51a872db11ef93aaa
abhishekver234 <163495242+abhishekver234@users.noreply.github.com>
2024-03-15 10:41:34 +0530

    Update as.py

--- as.py
+++ as.py
@@ -1,1 +1,10 @@
-print("Hello, world!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Abhishek Verma'
+
+if __name__ == '__main__':
+    app.run(debug=True)
